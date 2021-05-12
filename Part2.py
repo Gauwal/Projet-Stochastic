f608fe4834adae941c3fdf62c857ff6199d6a43f
Arthur Gengler <arthur.gengler@student.uclouvain.be>
2021-05-12 19:24:39 +0200

    DebutEnKF

--- Part2.py
+++ Part2.py
@@ -4,8 +4,6 @@ import matplotlib.pyplot as plt
 import math 
 
 
-
-
 #Lecture de u 
 file1 = open('Input_linear_case.txt', "r")
 lines = file1.readlines()
@@ -44,8 +42,7 @@ for line in lines:
         x1_true[i-1]=float(line.strip().split(" ")[0])
     i+=1
 file3.close()
-plt.figure(1)
-plt.plot(x1_true)
+
 #x2_true
 file4 = open('True_state_x1_linear_case.txt', "r")
 lines = file4.readlines()
@@ -58,8 +55,7 @@ for line in lines:
         x2_true[i-1]=float(line.strip().split(" ")[0])
     i+=1
 file4.close()
-plt.figure(2)
-plt.plot(x2_true)
+
 
 
 #Matrice of Kalman filter
@@ -69,7 +65,6 @@ B=np.array([0.0975,0.0024])
 B.shape=(2,1)
 C=np.array([0,1])
 C.shape=(1,2)
-
 
 Q=np.array([[10**(-3)*9.506,10**(-3)*0.0234],[10**(-3)*0.234,10**(-3)*9.512]])
 Q.shape=(2,2)
@@ -127,19 +122,82 @@ for i in range(0,k):
         mutilde=mu_tilde(A,B,mukprev,ucurrent)
         Ptilde=P_tilde(A,Q,Pprev)
         Kk=K_k(Ptilde,C,R)
-        print(Kk)
         muk=mu_k(mutilde,Kk,yk,C)
         Pk=P_k(Inx,Kk,C,Ptilde)
         x1[i]=np.random.normal(muk[0],Pk[0][0])
         x2[i]=np.random.normal(muk[1],Pk[1][1])
- 
-plt.figure(3)
-plt.plot(x1) 
-plt.figure(4)
-plt.plot(x2) 
-        
-        
-        
-        
-        
-        
+
+a=True
+#a=False    
+if(a):    
+    plt.figure(1)
+    plt.title("X1 True values")
+    plt.plot(x1_true)
+    plt.figure(2)
+    plt.title("X2 True values")
+    plt.plot(x2_true)
+    plt.figure(3)
+    plt.title("X1 Filtered values")
+    plt.plot(x1) 
+    plt.figure(4)
+    plt.title("X2 Filtered values")
+    plt.plot(x2)
+    plt.figure(5)
+    plt.title("Y")
+    plt.plot(y)
+
+
+###------EnKF------###
+
+
+def sample_mean(zer):
+    sum=0
+    for i in range(len(zer)):
+        sum=sum+zer[i]
+    return sum/len(zer)
+
+#faut Rajouter le bruit
+def x_tilde(A,B,ucurrent,x1_hat_i_kprev,x2_hat_i_kprev):
+    return A@[x1_hat_i_kprev,x2_hat_i_kprev]+B@ucurrent
+
+#faut Rajouter le bruit
+def y_tilde(C,xtilde):
+    return C@xtilde
+
+def K_k_approx(Papprox,C,R):
+    abc=1/(np.dot(np.dot(C,Papprox),np.transpose(C))+R)
+    return np.dot(Papprox,np.transpose(C))*abc     
+    
+    
+
+N=3
+x1_hat_prev=np.array(np.zeros((N,1)))
+x2_hat_prev=np.array(np.zeros((N,1)))
+
+print("x1_hat_prev",x1_hat_prev.shape)
+for j in range(0,1000):
+    for i in range(0,N):
+        if j==0:
+            x1_hat_prev[i][0]=np.random.normal(mu_x_init[0],Cov_x_init[0][0])
+            x2_hat_prev[i][0]=np.random.normal(mu_x_init[1],Cov_x_init[1][1])
+        else:
+            x1_hat_prev[i][0]=4
+            x2_hat_prev[i][0]=4
+            
+    print("x1_hat_prev",x1_hat_prev.shape)
+    x_hat_prev=np.array([[x1_hat_prev],[x2_hat_prev]])
+    x_hat_prev.shape=(2,1,3)
+    print("x_hat_prev",x_hat_prev.shape)
+    print("x_hat_prev",x_hat_prev)    
+    xtilde=A@x_hat_prev+B@u    #+bruit
+    ytilde=C@xtilde            #+bruit
+    Ptildeapprox=np.cov(xtilde)
+    Kkapprox=Ptildeapprox@C.T*(1/(C@Ptildeapprox@C.T+R))
+    x_hat=xtilde+Kkapprox*(yk-ytilde)
+    
+    
+    
+    
+    
+    
+    
